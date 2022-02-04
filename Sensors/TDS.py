29e148d0a193e84e1cb3faf8296e9ad8165ceed5
mp619 <mp619@ic.ac.uk>
2022-02-04 14:29:46 +0000

    TDS_Test

---
+++ Sensors/TDS.py
@@ -0,0 +1,32 @@
+from smbus2
+import time
+
+while True :
+    #get I2C bus
+    bus = smbus2.SMBus(1)
+    #State address of ADC
+    ADC_adr = 0x48
+    #ADC useful register pointers - Comparator registers not required
+    ADC_REG_PTR_Conv = 0x00 #Pointer to conversion register - note 16bits
+    ADC_REG_PTR_Conf = 0x01 #Pointer to Config register - note 16bits
+
+    #Configure ADC
+    ADC_Reg_Gain = 0x02 # A gain of 1, hence a v_ref of around 3V
+
+
+
+
+    #Write to Config Register for continuous conversion
+    Config_reg = smbus2.i2c_msg.write(ADC_adr,[ADC_REG_PTR_Conf,0x84,0x83])
+    Pointer_reg = smbus2.i2c_msg.write(ADC_adr,[ADC_REG_PTR_Conv])
+    bus.i2c_rdwr(Config_reg)
+    bus.i2c_rdwr(Pointer_reg)
+    #wait for measurement
+    time.sleep(0.1)
+    #send the read ADC command and read two bytes of data
+    Read_Conv = smbus2.i2c_msg.read(ADC_adr,2)
+    bus.i2c_rdwr(Read_Conv)
+    #convert the result to an int
+    temp = int.from_bytes(read_result.buf[0]+read_result.buf[1],"big")
+    time.sleep(0.1)
+    print(temp)
