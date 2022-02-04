176221bbcac0f55117117935542a869bb0c3d3f8
mp2419 <59872205+mp2419@users.noreply.github.com>
2022-02-04 12:47:10 +0000

    try

--- rasp_comm/test_client.py
+++ rasp_comm/test_client.py
@@ -32,25 +32,25 @@ def Json_create( SensorReading ):
 
 temp = []
 
-while False :
-    bus = smbus2.SMBus(1)
-    #send the measure temperature command
-    meas_temp= smbus2.i2c_msg.write(24,[0xAA,0x00,0x00])
-    bus.i2c_rdwr(meas_temp)
-    #wait for measurement
-    time.sleep(0.1)
-    #send the read temperature command and read two bytes of data
-    read_result= smbus2.i2c_msg.read(24,4)
-    bus.i2c_rdwr(read_result)
-    #convert the result to an int
-    temp.append(int.from_bytes(read_result.buf[0]+read_result.buf[1]+read_result.buf[2]+read_result.buf[3], "big"))
-    time.sleep(0.1)
-    print(temp)
-    if len(temp)== 5:
-        json_output = Json_create( temp)
-        temp = []
-        print(json_output)
-        MSG_INFO= client.publish("IC.embedded/M2S2/test",json_output)
-        print(mqtt.error_string(MSG_INFO.rc))
+# while True :
+#     bus = smbus2.SMBus(1)
+#     #send the measure temperature command
+#     meas_temp= smbus2.i2c_msg.write(24,[0xAA,0x00,0x00])
+#     bus.i2c_rdwr(meas_temp)
+#     #wait for measurement
+#     time.sleep(0.1)
+#     #send the read temperature command and read two bytes of data
+#     read_result= smbus2.i2c_msg.read(24,4)
+#     bus.i2c_rdwr(read_result)
+#     #convert the result to an int
+#     temp.append(int.from_bytes(read_result.buf[0]+read_result.buf[1]+read_result.buf[2]+read_result.buf[3], "big"))
+#     time.sleep(0.1)
+#     print(temp)
+#     if len(temp)== 5:
+#         json_output = Json_create( temp)
+#         temp = []
+#         print(json_output)
+#         MSG_INFO= client.publish("IC.embedded/M2S2/test",json_output)
+#         print(mqtt.error_string(MSG_INFO.rc))
 
 client.loop()
