5ae21e6bd5cd64cc8c0ef41e50f35cc570204cd2
mp619 <martin_prusa@outlook.com>
2022-02-08 16:11:45 +0000

    Sensor Tests Done

--- Sensors/RGB.py
+++ Sensors/RGB.py
@@ -1,6 +1,12 @@
+import time
 from gpiozero import RGBLED, Button
-from colorzero import color
+from colorzero import Color
 
+led = RGBLED(22, 27, 17, False) # This is a common anode type RBG LED
+button = Button(18, False) # The Raspi will pull up the button
 while True:
-    led = RGBLED(22, 27, 17)
-    led.color = Color('yellow')
+    if button.is_held:
+        led.color = Color('yellow')
+    else:
+        led.off()
+    
