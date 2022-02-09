8e8269ae464d47a41f1323dc2b5b97f741d46861
mp619 <mp619@ic.ac.uk>
2022-02-09 10:28:57 +0000

    TDS_MQTT

---
+++ Sensors/TDS_SendData.py
@@ -0,0 +1,60 @@
+import datetime
+import json
+from dateutil.tz import tzutc
+import smbus2
+import time
+import paho.mqtt.client as mqtt
+
+client = mqtt.Client()
+client.tls_set(ca_certs="mosquitto.org.crt",certfile="client.crt",keyfile="client.key")
+client.connect("test.mosquitto.org",port=1883)
+
+def Json_create( SensorReading ):
+    a_datetime = datetime.datetime.now()
+    time_stamp = a_datetime.isoformat()
+    Reading_dic = {
+        'TimeStamp': time_stamp,
+        'SensorReading': SensorReading
+    }
+    json_output = json.dumps(Reading_dic)
+    return json_output
+
+temp = []
+bus = smbus2.SMBus(1)
+
+while True :
+    
+    #State address of ADC
+    ADC_adr = 0x48
+    #ADC useful register pointers - Comparator registers not required
+    ADC_REG_PTR_Conv = 0x00 #Pointer to conversion register - note 16bits
+    ADC_REG_PTR_Conf = 0x01 #Pointer to Config register - note 16bits
+
+    #Configure ADC
+    ADC_Reg_Gain = 0x02 # A gain of 1, hence a v_ref of around 3V
+
+    #Write to Config Register for single-shot conversion
+    Config_reg = smbus2.i2c_msg.write(ADC_adr,[ADC_REG_PTR_Conf,0xC2,0x83])
+    Pointer_reg = smbus2.i2c_msg.write(ADC_adr,[ADC_REG_PTR_Conv])
+    bus.i2c_rdwr(Config_reg)
+    bus.i2c_rdwr(Pointer_reg)
+    #wait for measurement
+    time.sleep(0.1)
+    #send the read ADC command and read two bytes of data
+    Read_Conv = smbus2.i2c_msg.read(ADC_adr,2)
+    bus.i2c_rdwr(Read_Conv)
+    #convert the result to an int and turn negative numbers to 0
+    temp = int.from_bytes(Read_Conv.buf[0]+Read_Conv.buf[1],"big")
+    if temp > 32767:
+	    temp = temp - 65535
+    temp = temp*0.125	# Convert to mv
+    temp = temp*0.001   # Convert to V
+    #print binary values
+    time.sleep(0.1)
+    print(temp)
+    if len(temp)== 5:
+        json_output = Json_create( temp)
+        temp = []
+        print(json_output)
+        MSG_INFO= client.publish("IC.embedded/M2S2/test",json_output)
+        print(mqtt.error_string(MSG_INFO.rc))
