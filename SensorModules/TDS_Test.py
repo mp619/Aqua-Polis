d7ca7d0b45e3ceab7da44d3597e0d1ec42befb64
mp619 <mp619@ic.ac.uk>
2022-02-23 11:33:46 +0000

    Testing Sending

--- SensorModules/TDS_Test.py
+++ SensorModules/TDS_Test.py
@@ -7,24 +7,20 @@ ADC_adr = 0x48
 
 bus = TDS.init()
 TDS.config(bus, ADC_adr)
-Turb.config(bus, ADC_adr)
 
 while True:
     total_TDS = 0
     total_Turb = 0
     for i in range(0,10):
-        TDS.config(bus, ADC_adr)
         t1 = TDS.read(bus, ADC_adr)
-        print(t1)
-        time.sleep(1)
-        #Turb.config(bus, ADC_adr)
-        #t2 = Turb.read(bus, ADC_adr)
-        #print(t2)
-        time.sleep(1)
         total_TDS = total_TDS + t1
-        #total_Turb = total_Turb + t2
     avg_TDS = total_TDS/10
-    #avg_Turb = total_Turb/10
+    Turb.config(bus, ADC_adr)
+    time.sleep(0.1)
+    for i in range(0,10):
+        t2 = Turb.read(bus, ADC_adr)
+        total_Turb = total_Turb + t2
+    avg_Turb = total_Turb/10
     tds = TDS.convert(avg_TDS)
-    #turb = Turb.convert(avg_Turb)
+    turb = Turb.convert(avg_Turb)
     print(tds, 'ppm', turb, 'NTU') 
